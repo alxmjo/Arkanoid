08aa6adbf9c0fa66f9861c1cb70633a639f867d1
Alex Johnson <alexmontjohn@gmail.com>
2017-03-18 16:44:40 +0300

    Part 3

--- main.cpp
+++ main.cpp
@@ -6,8 +6,34 @@ using namespace sf;
 
 // constexpr defines an immutable compile-time value
 constexpr int windowWidth{800}, windowHeight{600};
+constexpr float ballRadius{10.0f}, ballVelocity{8.0f};
+
+struct Ball {
+    // CircleShape is an SFML class that defines a renderable circle
+    CircleShape shape;
+
+    // 2D vector that stores the Ball's velocity
+    Vector2f velocity{-ballVelocity, -ballVelocity};
+
+    // Let's create the Ball constructor
+    // argument posX -> starting x coordinate
+    // argument posY -> starting y coordinate
+    Ball(float posX, float posY) {
+        // Apply position, radius, color, and origin to the CircleShape shape
+        shape.setPosition(posX, posY);
+        shape.setRadius(ballRadius);
+        shape.setFillColor(Color::Red);
+        shape.setOrigin(ballRadius, ballRadius);
+    }
+
+    // Update the ball, move its shape by the current velocity
+    void update() { shape.move(velocity); }
+};
 
 int main() {
+    // Create an instance of Ball positioned at the center of the window
+    Ball ball {windowWidth / 2, windowHeight / 2};
+
     // Creation of the game window
     RenderWindow window{{windowWidth, windowHeight}, "Arkanoid - 1"};
     window.setFramerateLimit(60);
@@ -24,7 +50,11 @@ int main() {
         // If Escape is pressed, break out of the loop
         if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
 
+        // Update the ball
+        ball.update();
+
         // Show the window contents
+        window.draw(ball.shape);
         window.display();
     }
 
