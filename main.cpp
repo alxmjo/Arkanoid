ef7938a7ed2dbe88997d771bdc8de3c403be46c6
Alex Johnson <alexmontjohn@gmail.com>
2017-03-19 04:54:52 +0300

    Part 5

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ using namespace sf;
 constexpr int windowWidth{800}, windowHeight{600};
 constexpr float ballRadius{10.0f}, ballVelocity{8.0f};
 
+// Create constants for paddle
+constexpr float paddleWidth{60.0f}, paddleHeight{20.0f}, paddleVelocity{6.0f};
+
 struct Ball {
     // CircleShape is an SFML class that defines a renderable circle
     CircleShape shape;
@@ -55,9 +58,46 @@ struct Ball {
     float bottom()    { return y() + shape.getRadius(); }
 };
 
+struct Paddle {
+    RectangleShape shape;
+    Vector2f velocity;
+
+    Paddle(float posX, float posY) {
+        shape.setPosition(posX, posY);
+        shape.setSize({paddleWidth, paddleHeight});
+        shape.setFillColor(Color::Red);
+        shape.setOrigin(paddleWidth / 2.0f, paddleHeight / 2.0f);
+    }
+
+    void update() {
+        shape.move(velocity);
+
+        // Use keyboard unput to change paddle position
+        if (Keyboard::isKeyPressed(Keyboard::Key::Left) && left() > 0) {
+            velocity.x = -paddleVelocity;
+        } else if (Keyboard::isKeyPressed(Keyboard::Key::Right) && right() < windowWidth) {
+            velocity.x = paddleVelocity;
+        } else {
+            velocity.x = 0;
+        }
+
+
+    }
+
+    float x()       { return shape.getPosition().x; }
+    float y()       { return shape.getPosition().y; }
+    float left()    { return x() - shape.getSize().x / 2.0f; }
+    float right()   { return x() + shape.getSize().x / 2.0f; }
+    float top()     { return y() - shape.getSize().y / 2.0f; }
+    float bottom()  { return y() + shape.getSize().y / 2.0f; }
+};
+
 int main() {
     // Create an instance of Ball positioned at the center of the window
     Ball ball {windowWidth / 2, windowHeight / 2};
+
+    // Create paddle and center in window
+    Paddle paddle{ windowWidth / 2, windowHeight - 50};
 
     // Creation of the game window
     RenderWindow window{{windowWidth, windowHeight}, "Arkanoid - 1"};
@@ -78,8 +118,12 @@ int main() {
         // Update the ball
         ball.update();
 
+        // Update the paddle
+        paddle.update();
+
         // Show the window contents
         window.draw(ball.shape);
+        window.draw(paddle.shape);
         window.display();
     }
 
